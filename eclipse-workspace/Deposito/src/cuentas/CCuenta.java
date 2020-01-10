667cf3388eedde0c20aa58f150a4daaeec1b92df
macawan <59702220+macawan@users.noreply.github.com>
2020-01-10 10:53:43 -0300

    Deposito repositorio

---
+++ eclipse-workspace/Deposito/src/cuentas/CCuenta.java
@@ -0,0 +1,73 @@
+package cuentas;
+public class CCuenta {
+
+
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInteres;
+
+    public CCuenta()
+    {
+    }
+
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        setNombre(nom);
+        setCuenta(cue);
+        setSaldo(sal);
+    }
+
+    public double estado()
+    {
+        return getSaldo();
+    }
+
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        setSaldo(getSaldo() + cantidad);
+    }
+
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (estado()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        setSaldo(getSaldo() - cantidad);
+    }
+
+	protected String getNombre() {
+		return nombre;
+	}
+
+	protected void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	protected String getCuenta() {
+		return cuenta;
+	}
+
+	protected void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	protected double getSaldo() {
+		return saldo;
+	}
+
+	protected void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	protected double getTipoInteres() {
+		return tipoInteres;
+	}
+
+	protected void setTipoInteres(double tipoInteres) {
+		this.tipoInteres = tipoInteres;
+	}
+}
